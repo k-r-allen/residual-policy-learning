43cd575e980c96e030ce76765b0e5946abc05f34
Tom Silver <tom@Toms-MacBook-Pro-2.local>
2018-12-23 09:48:41 -0500

    minor (fixing spacing)

--- tensorflow/experiment/submit_all_experiments.sh
+++ tensorflow/experiment/submit_all_experiments.sh
@@ -15,27 +15,27 @@ num_experiments=$2
 experiment_counter=0
 
 for j in ${!seeds[@]}; do
-	#Train expert-explore
-	for i in ${!expertexplore[@]}; do
+    #Train expert-explore
+    for i in ${!expertexplore[@]}; do
       if (( experiment_counter >= start && experiment_counter < (start + num_experiments) )); then
-	    python train_staged.py --env ${expertexplore[$i]} --n_epochs ${nepochs[$i]} --num_cpu ${cpus[$i]} --config_path=configs/${configs[$i]} --logdir ./logs/seed${seeds[$j]}/${expertexplore[$i]}_expertexplore --seed ${seeds[$j]} --random_eps=${eps[$i]} --controller_prop=${alpha[$i]}
+        python train_staged.py --env ${expertexplore[$i]} --n_epochs ${nepochs[$i]} --num_cpu ${cpus[$i]} --config_path=configs/${configs[$i]} --logdir ./logs/seed${seeds[$j]}/${expertexplore[$i]}_expertexplore --seed ${seeds[$j]} --random_eps=${eps[$i]} --controller_prop=${alpha[$i]}
       fi
       ((experiment_counter++))
-	done
+    done
 
-	#Train from scratch
-	for i in ${!envs[@]}; do
+    #Train from scratch
+    for i in ${!envs[@]}; do
       if (( experiment_counter >= start && experiment_counter < (start + num_experiments) )); then
         python train_staged.py --env ${envs[$i]} --n_epochs ${nepochs[$i]} --num_cpu ${cpus[$i]} --config_path=configs/${configs[$i]} --logdir ./logs/seed${seeds[$j]}/${envs[$i]} --seed ${seeds[$j]}
       fi
       ((experiment_counter++))
-	done
+    done
 
-	#Train residuals
-	for i in ${!residuals[@]}; do
+    #Train residuals
+    for i in ${!residuals[@]}; do
       if (( experiment_counter >= start && experiment_counter < (start + num_experiments) )); then
         python train_staged.py --env ${residuals[$i]} --n_epochs ${nepochs[$i]} --num_cpu ${cpus[$i]} --config_path=configs/${configs[$i]} --logdir ./logs/seed${seeds[$j]}/${residuals[$i]} --seed ${seeds[$j]}
       fi
       ((experiment_counter++))
-	done
+    done
 done
