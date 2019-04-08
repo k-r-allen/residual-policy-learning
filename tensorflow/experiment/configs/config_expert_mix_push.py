958e5e4a57616ce260fcb0a6504a0a5ea38c456d
k-r-allen <krallen@mit.edu>
2019-04-07 22:46:13 -0400

    updates to configs

--- tensorflow/experiment/configs/config_expert_mix_push.py
+++ tensorflow/experiment/configs/config_expert_mix_push.py
@@ -1,7 +1,6 @@
 import numpy as np
 import gym
 import rpl_environments
-import gym_residual_complex_objects
 from baselines import logger
 from ddpg_controller import DDPG
 from baselines.her.her import make_sample_her_transitions
